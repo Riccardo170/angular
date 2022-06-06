192413944e16f140877e8bff28073684df87baf9
lai2-01 <lai2-01@itismeucci.com>
2022-06-06 11:48:34 +0200

    da finire

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LearnTableComponent } from './learn-table/learn-table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LearnTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
